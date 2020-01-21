03610e0e752b2585942940e7a683a1b82dfc7ff0
rafalkarny <rafal.karnowka@gmail.com>
2020-01-21 23:29:24 +0100

    Dodano funkcjonalność listowania projektów i otwierania ich okien

--- AutomationProjectManager/DataModels/BoardPoco.cs
+++ AutomationProjectManager/DataModels/BoardPoco.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace AutomationProjectManager.Model
 {
@@ -12,5 +14,8 @@ namespace AutomationProjectManager.Model
         public string Name { get; set; }
         public int ProjectId { get; set; }
         public int RoleAccess { get; set; }
+    
+    
     }
+
 }
--- AutomationProjectManager/ProjectWindow.xaml.cs
+++ AutomationProjectManager/ProjectWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutomationProjectManager.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,26 @@ namespace AutomationProjectManager
     /// </summary>
     public partial class ProjectWindow : Window
     {
-        public ProjectWindow()
+        public ProjectWindow(int ProjectId)
         {
             InitializeComponent();
+            updateComboBox(ProjectId);
+        }
+
+        public void updateComboBox(int ProjectId)
+        {
+            RestClient client = new RestClient();
+            client.method = httpVerb.GET;
+            client.serviceUri = "https://localhost:44309/api/Boards/"+ProjectId.ToString(); //W serwisie musiałaby być 0- to id projektu do którego board należy
+            string response = client.getRequest();
+            List<BoardPoco> boardList;
+            boardList = JsonConvert.DeserializeObject<List<BoardPoco>>(response);
+
+            foreach(BoardPoco board in boardList)
+            {
+                selectBoardBox.Items.Add(board.Name);
+            }
+           
         }
     }
 }
