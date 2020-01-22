93abb76a9678a5052602483fcde889bae10d4c24
PanWilson <panwilsonn@gmail.com>
2020-01-22 23:41:56 +0100

    TaskPoco derived classes

---
+++ AutomationProjectManager/DataModels/TasksChildrens/ElectricalProject.cs
@@ -0,0 +1,31 @@
+﻿using AutomationProjectManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationProjectManager.DataModels.TasksChildrens
+{
+    public class ElectricalProject : TaskPoco
+    {
+        public ElectricalProject()
+        {
+            this.BoardId = -1;
+            this.TaskId = -1;
+            this.Type = TaskType.DriversProject;
+        }
+
+        public ElectricalProject(int BoardId, string Content, int TaskId)
+        {
+            this.BoardId = BoardId;
+            this.TaskId = TaskId;
+            this.Type = TaskType.ProjectDescription;
+        }
+
+        public override void UpdateContent()
+        {
+
+        }
+    }
+}
---
+++ AutomationProjectManager/DataModels/TasksChildrens/Mounting.cs
@@ -0,0 +1,31 @@
+﻿using AutomationProjectManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationProjectManager.DataModels.TasksChildrens
+{
+    public class Mounting : TaskPoco
+    {
+        public Mounting()
+        {
+            this.BoardId = -1;
+            this.TaskId = -1;
+            this.Type = TaskType.DriversProject;
+        }
+
+        public Mounting(int BoardId, string Content, int TaskId)
+        {
+            this.BoardId = BoardId;
+            this.TaskId = TaskId;
+            this.Type = TaskType.ProjectDescription;
+        }
+
+        public override void UpdateContent()
+        {
+
+        }
+    }
+}
