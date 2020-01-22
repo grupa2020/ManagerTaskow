c9dae00032dc44a1f6f557fad6f1f4a41183d7f6
PanWilson <panwilsonn@gmail.com>
2020-01-22 19:50:57 +0100

    ProjektLables

---
+++ AutomationProjectManager/ProjectLabel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutomationProjectManager
+{
+    /// <summary>
+    /// Interaction logic for ProjectLabel.xaml
+    /// </summary>
+    public partial class ProjectLabel : UserControl
+    {
+        public ProjectLabel()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AutomationProjectManager/ProjektListWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutomationProjectManager
+{
+    /// <summary>
+    /// Interaction logic for ProjektListWindow.xaml
+    /// </summary>
+    public partial class ProjektListWindow : UserControl
+    {
+        public ProjektListWindow()
+        {
+            InitializeComponent();
+            InitProjects();
+        }
+
+        void InitProjects()
+        {
+            for(int i =0; i< 3;i++)
+            {
+                ProjectLabel pLable = new ProjectLabel();
+                ProjectsList.Children.Add(pLable);
+            }
+        }
+
+    }
+}
