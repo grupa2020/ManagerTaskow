1e3283d716fa980f54fa01b555071e361875eaa1
wojechklim <59651028+wojechklim@users.noreply.github.com>
2020-02-01 19:17:50 +0100

    Add files via upload

---
+++ AutomationProjectManager/DataModels/KKS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationProjectManager.ToolsWindows
+{
+    class KKS
+    {
+        public string Indeks { get; set; }
+        public string TypZmiennej { get; set; }
+        public string SymbolUrzadzenia { get; set; }
+        public string SymbolSzafy { get; set; }
+        public string Opis { get; set; }
+        public string Komentarz { get; set; }
+
+        public KKS(string Id, string TpZ, string TypU, string SmbSz, string Ops, string Kom)
+        {
+            Indeks = Id;
+            TypZmiennej = TpZ;
+            SymbolUrzadzenia = TypU;
+            SymbolSzafy = SmbSz;
+            Opis = Ops;
+            Komentarz = Kom;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3} {4} {5}", Indeks, TypZmiennej, SymbolUrzadzenia, SymbolSzafy, Opis, Komentarz);
+        }
+    }
+}
+
